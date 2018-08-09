73a591b4e7d86f561e3fdafbf68827a8222ffa11
danielcruser <danielcruser1@gmail.com>
2018-08-09 17:04:33 -0400

    add error handling for incorrect selectors

--- normalizrResolver.js
+++ normalizrResolver.js
@@ -2,7 +2,12 @@ import { normalize } from 'normalizr';
 
 const defaultNormalizrResolver = (action, actionResolver) => {
   const { slice, schema } = actionResolver[action.type](action);
-  return normalize(slice(action), schema);
+  const data = slice(action)
+  if (!data) {
+    throw new Error(`You are trying to normalize undefined. You probably are selecting the wrong part of your action. Make sure that the path specified in ${slice} is correct`)
+  } else {
+    return normalize(slice(action), schema);
+  }
 };
 
 export default defaultNormalizrResolver;
